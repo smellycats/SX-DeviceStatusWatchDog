4c42d5de150f978dc707ed089edf3323db25f574
SmellyCat <smellycat2014@foxmail.com>
2017-03-20 15:05:56 +0800

    更改 根据type分开发送设备信息

--- watch_dog.py
+++ watch_dog.py
@@ -24,26 +24,33 @@ class WatchDog(object):
         
         self.sms = SMS(**self.my_ini.get_sms())
         self.dev = Device(**self.my_ini.get_device())
+        self.dev.base_path = ''
 	
 	# 设备状态字典 {'127.0.0.1': '2017-02-03 12:00:00'}
         self.device_status_dict = {}
-        # 设备连接失败集合
-        self.device_false_set = set()
+        # 设备连接失败字典
+        self.device_false_dict = {
+            1: set(),
+            2: set(),
+            3: set()
+        }
         # 短信发送记录，形如{('441302001', 'IN'): <Arrow [2016-03-02T20:08:58.190000+08:00]>}
         self.mobiles_list = []
         # 短信发送时间间隔
         self.send_time_step = 12
+	# 时间标记
+	self.time_flag = arrow.now()
 
     def __del__(self):
         pass
 
-    def get_device_dict(self):
+    def get_device_dict(self, type=None):
         """IP地址字典"""
         # 设备信息字典
         device_dict = {}
         # 设备IP无法连接集合
         device_false_set = set()
-        dev = self.dev.get_device_list()
+        dev = self.dev.get_device_list(type)
         for i in dev['items']:
             device_dict[i['ip']] = i
             if not i['status']:
@@ -51,21 +58,20 @@ class WatchDog(object):
         return device_dict, device_false_set
 
 
-    def device_status_check(self):
-        device_dict, device_false_set = self.get_device_dict()
-
-        if self.device_false_set == set():
-            self.device_false_set = device_false_set
-        else:
-            device_true_set = device_false_set - self.device_false_set
+    def device_status_check(self, type=None):
+        device_dict, device_false_set = self.get_device_dict(type)
+        # 恢复设备集合
+        device_true_set = device_false_set - self.device_false_dict[type]
+        self.device_false_dict[type] = device_false_set
 
         # 恢复短信发送列表
         sms_send_list = []
         for i in list(device_true_set):
             sms_send_list.append(device_dict[i])
+            del self.device_status_dict[i]   # 删除IP对应发送状态
         self.sms_send_info(sms_send_list, status=True)
 
-        # 失败短信发送列表
+        # 断开短信发送列表
         sms_send_list2 = []
         # 当前时间
         now = arrow.now()
@@ -82,7 +88,6 @@ class WatchDog(object):
                 sms_send_list2.append(device_dict[i])
         self.sms_send_info(sms_send_list2, status=False)
 
-
     def sms_send_info(self, sms_send_list, status=False):
         """发送短信通知"""
 	if sms_send_list == []:
@@ -98,23 +103,22 @@ class WatchDog(object):
 
         self.sms.sms_send(content, self.mobiles_list)
 
-
-        
-##    def run(self):
-##        print 'start run'
-##        while 1:
-##            try:
-##                # 当前时间
-##                t = arrow.now()
-##                # 每30秒检查一遍
-##                if t > self.time_flag.replace(seconds=30):
-##                    self.device_status_check()
-##                    self.time_flag = t
-##            except Exception as e:
-##                logger.exception(e)
-##                time.sleep(10)
-##            finally:
-##                time.sleep(1)
+    def run(self):
+        print 'start run'
+        while 1:
+            try:
+                # 当前时间
+                t = arrow.now()
+                # 每30秒检查一遍
+                if t > self.time_flag.replace(seconds=30):
+                    for i in [2, 3]:
+                        self.device_status_check(i)
+                    self.time_flag = t
+            except Exception as e:
+                logger.exception(e)
+                time.sleep(15)
+            finally:
+                time.sleep(1)
 
 if __name__ == "__main__":
     wd = WatchDog()
