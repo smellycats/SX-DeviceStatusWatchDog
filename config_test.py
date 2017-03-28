c3e5782baa61553c6007e767be9971afcf28fdd5
SmellyCat <smellycat2014@foxmail.com>
2017-03-28 23:12:27 +0800

    添加 yaml方法

--- config_test.py
+++ config_test.py
@@ -1,24 +1,31 @@
 #-*- encoding: utf-8 -*-
-from ini_conf import MyIni
+from my_yaml import MyYAML
 
 
-class ConfigTest(object):
+class TestYAML(object):
     def __init__(self):
-        self.my_ini = MyIni()
+        self.my_ini = MyYAML()
         
-    def test_sms(self):
-        print self.my_ini.get_sms()
-        
-    def test_ping(self):
-        print self.my_ini.get_ping()
+    def get_ini(self):
+        print self.my_ini.get_ini()
+        print list(self.my_ini.get_ini()['mobiles'])
 
-    def test_device(self):
-        print self.my_ini.get_device()
+    def set_ini(self):
+        data = self.my_ini.get_ini()
+        print 'data=' % data
+        data['mobiles'] = [
+            '123',
+            '678'
+        ]
+        print self.my_ini.set_ini(data) 
 
 
 
 if __name__ == "__main__":
-    ct = ConfigTest()
-    ct.test_sms()
-    ct.test_ping()
-    ct.test_device()
+    #ct = ConfigTest()
+    #ct.test_sms()
+    #ct.test_ping()
+    ty = TestYAML()
+    ty.get_ini()
+    #ty.set_ini()
+    del ty
